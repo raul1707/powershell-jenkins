8dd062e5d8a384ddfa73a22d76f82d289feb73c3
raul1707 <135829085+raul1707@users.noreply.github.com>
2023-08-10 20:57:52 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,11 @@
 #!/usr/bin/pwsh -Command
-echo "Hello World!"
+New-AzVm `
+    -ResourceGroupName 'myResourceGroup' `
+    -Name 'myVM' `
+    -Location 'East US' `
+    -VirtualNetworkName 'myVnet' `
+    -SubnetName 'mySubnet' `
+    -SecurityGroupName 'myNetworkSecurityGroup' `
+    -PublicIpAddressName 'myPublicIpAddress' `
+    -OpenPorts 80,3389
 
